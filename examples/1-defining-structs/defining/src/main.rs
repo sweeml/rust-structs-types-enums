1d0ae598b79038d2d99884bb40e2f3cfce920601
Michael Sweeney <87456288+sweeml@users.noreply.github.com>
2024-04-22 20:44:17 +0000

    Created an impl of Person to concat first & last name

--- examples/1-defining-structs/defining/src/main.rs
+++ examples/1-defining-structs/defining/src/main.rs
@@ -4,6 +4,14 @@ struct Person {
     first_name: String,
     last_name: String,
     age: u8,
+    email: String,
+    phone_number: String,
+}
+
+impl Person {
+    fn full_name(&self) -> String {
+        format!("{} {}", self.first_name, self.last_name)
+    }
 }
 
 fn main() {
@@ -11,5 +19,7 @@ fn main() {
         first_name: "John".to_string(),
         last_name: "Doe".to_string(),
         age: 25,
+        email: "test@test.com".to_string(),
+        phone_number: "+1234567890".to_string(),
     });
 }
