17058a140cf86c4b40008fa7b1f70df406b93ffd
Michael Sweeney <87456288+sweeml@users.noreply.github.com>
2024-04-23 20:48:26 +0000

    Created a function that creates a user instance from an email address

--- examples/3-associated-functions/associated/src/main.rs
+++ examples/3-associated-functions/associated/src/main.rs
@@ -17,14 +17,23 @@ impl User {
     fn deactivate(&mut self) {
         self.active = false;
     }
+
+    fn from_email(email: String) -> Option<Self> {
+        // Logic to retrieve user details from the email address
+        let username = email.split('@').next().unwrap_or_default().to_string();
+        
+        // If user details are found, create and return a new User instance
+        Some(Self {
+            username,
+            email,
+            uri: String::from("https://example.com"),
+            active: true,
+        })
+    }
 }
 
 fn main() {
-    let mut new_user = User::new(
-        String::from("alfredodeza"),
-        String::from("alfreodeza@example.com"),
-        String::from("https://alfredodeza.com"),
-    );
+    let mut new_user = User::from_email("mlsween55@gmail.com".to_string()).unwrap();
     println!("Hello, {}!", new_user.username);
     println!("Account {} status is: {}", new_user.username, new_user.active);
     new_user.deactivate();
